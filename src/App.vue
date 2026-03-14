65cc627c32bc858ebd46a4d0f82daa5fc0e9d05d
rachkb <rachkbg@gmail.com>
2026-03-14 13:55:37 +0800

    Initial commit: Vue + Tailwind setup

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-extrabold text-purple-600 drop-shadow-md">
+      Tailwind is Active!
+    </h1>
+  </div>
+</template>
